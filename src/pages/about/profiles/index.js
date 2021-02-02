2404b25a3bca90b831f6feec27d06d71763b80e8
Ahsan Tawil <ahsan.tawil07@gmail.com>
2021-02-02 11:12:35 +0700

    add vision and mission company

--- src/pages/about/profiles/index.js
+++ src/pages/about/profiles/index.js
@@ -48,6 +48,49 @@ export default class Profiles extends Component {
                             </div>
                         </div>
                     </section>
+                    <section id="about" className="about">
+                        <div className="container">
+                            <div className="row content">
+                                <div className="col-lg-6">
+                                    <h2>PT Citra Sanxing Indonesia</h2>
+                                    <h3>
+                                        is a company in motion
+                                        in manufacturing Energy Meter "kWh Meter" under the brand name
+                                        trade <i><u><b>"SANXING"</b></u></i>  which has met national standards as well
+                                        international. The products we produce are under one system
+                                        quality management in order to ensure the product production process
+                                        uniform and restrained.
+                                    </h3>
+                                </div>
+                                <div className="col-lg-6 pt-4 pt-lg-0">
+                                    <h3>VISION</h3>
+                                    <ul>
+                                    <li><i className="ri-check-double-line" /> Become the number one supplier of energy meter "kWh Meter" in Indonesia</li>
+                                    <li><i className="ri-check-double-line" /> 
+                                        Making kWh Meter that is able to compete with competitors others in the International market
+                                    </li>
+                                    <li><i className="ri-check-double-line" /> 
+                                        Maintaining Core Business of PT. Citra Sanxing Indonesia with expand cooperation to foreign countries
+                                    </li>
+                                    </ul>
+
+                                    <h3>MISION</h3>
+                                    <ul>
+                                    <li><i className="ri-check-double-line" /> Making quality kWh meters according to consumer needs PLN</li>
+                                    <li><i className="ri-check-double-line" />
+                                         Maintain the kWh Meter production so that the quality and quantity are appropriate with consumer demands   
+                                    </li>
+                                    <li><i className="ri-check-double-line" /> 
+                                        Increase the "Development Design" and "Technology" use support the competition in the business world
+                                    </li>
+                                    <li><i className="ri-check-double-line" /> 
+                                        Increase customer satisfaction
+                                    </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </main>
             </>
         )
